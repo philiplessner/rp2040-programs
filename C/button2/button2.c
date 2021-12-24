fa3651f40d3620e9f3314f2b80ad2a3420cd171f
Philip Lessner <philiplessner@gmail.com>
2021-12-23 21:27:38 -0500

    Button with hardware debounce

--- C/button2/button2.c
+++ C/button2/button2.c
@@ -3,15 +3,15 @@
 #include "pico/stdlib.h"
 
 int main() {
-    const uint BUTTON_PIN = 16;
+    const uint BUTTON_PIN = 17;
     gpio_init(BUTTON_PIN); 
     gpio_set_dir(BUTTON_PIN, GPIO_IN);
-    gpio_pull_down(BUTTON_PIN);
+    gpio_pull_up(BUTTON_PIN);
     stdio_init_all();
     while (true) {
-        if (gpio_get(BUTTON_PIN)) {
+        if (!gpio_get(BUTTON_PIN)) {
             printf("Button Pressed\n");
-            sleep_ms(500);
+            sleep_ms(150);
         }
     }
 }
