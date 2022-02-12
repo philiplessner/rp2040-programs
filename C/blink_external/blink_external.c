11b4da11d978d002e1e3c0de88f5dbb01442b6af
Philip Lessner <philiplessner@gmail.com>
2022-02-12 15:27:16 -0500

    Traffic Light using timer interrupt

--- C/blink_external/blink_external.c
+++ C/blink_external/blink_external.c
@@ -1,21 +1,43 @@
 // Blink an external LED
 #include "pico/stdlib.h"
 
+const uint LEDRed = 8;
+const uint LEDYellow = 9;
+const uint LEDGreen = 10;
+
 bool repeating_timer_callback(struct repeating_timer *t){
-    gpio_xor_mask(3<<8U);
+    static uint32_t count;
+
+    count++;
+
+    switch (count){
+        case 1:
+            gpio_put(LEDRed, 1);
+            gpio_put(LEDYellow, 0);
+            gpio_put(LEDGreen, 0);
+            break;
+        case 2:
+            gpio_put(LEDRed, 0);
+            gpio_put(LEDYellow, 1);
+            gpio_put(LEDGreen, 0);
+            break;
+        case 3:
+            gpio_put(LEDRed, 0);
+            gpio_put(LEDYellow, 0);
+            gpio_put(LEDGreen, 1);
+            break;
+        default:
+            count = 0;
+    }
     return true;
 }
 
 int main() {
-    const uint LEDRed = 8;
-    const uint LEDYellow = 9;
+    const uint32_t mask = 1U<<LEDRed | 1U<<LEDYellow | 1U<<LEDGreen;
     const uint32_t delay = 500;
-    gpio_init(LEDRed);
-    gpio_init(LEDYellow);
-    gpio_set_dir(LEDRed, GPIO_OUT);
-    gpio_set_dir(LEDYellow, GPIO_OUT);
-    gpio_put(LEDRed, 1);
-    gpio_put(LEDYellow, 0);
+    gpio_init_mask(mask);
+    gpio_set_dir_out_masked(mask);
+    gpio_put_masked(mask, 0);
     struct repeating_timer timer;
     add_repeating_timer_ms(delay, repeating_timer_callback, NULL, &timer);
     while (true) {
