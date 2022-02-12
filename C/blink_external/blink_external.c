bb8f825baf4472d119442b318d8ee2211e6070c4
Philip Lessner <philiplessner@gmail.com>
2022-02-12 10:40:28 -0500

    Updated cmake min ver to 3.13. Using timer irq for blinking

--- C/blink_external/blink_external.c
+++ C/blink_external/blink_external.c
@@ -1,14 +1,24 @@
 // Blink an external LED
 #include "pico/stdlib.h"
 
+bool repeating_timer_callback(struct repeating_timer *t){
+    gpio_xor_mask(3<<8U);
+    return true;
+}
+
 int main() {
-    const uint LED_PIN = 15;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+    const uint LEDRed = 8;
+    const uint LEDYellow = 9;
+    const uint32_t delay = 500;
+    gpio_init(LEDRed);
+    gpio_init(LEDYellow);
+    gpio_set_dir(LEDRed, GPIO_OUT);
+    gpio_set_dir(LEDYellow, GPIO_OUT);
+    gpio_put(LEDRed, 1);
+    gpio_put(LEDYellow, 0);
+    struct repeating_timer timer;
+    add_repeating_timer_ms(delay, repeating_timer_callback, NULL, &timer);
     while (true) {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(250);
+        tight_loop_contents();
     }
 }
