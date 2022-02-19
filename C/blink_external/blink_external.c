d00ae2cb69df2cc543a26549c1bcaa7c4786a302
Philip Lessner <philiplessner@gmail.com>
2022-02-19 16:58:24 -0500

    Added Blue LED blink in main loop

--- C/blink_external/blink_external.c
+++ C/blink_external/blink_external.c
@@ -1,9 +1,13 @@
-// Blink 3 external LEDs using timer interrupt
+/* Blink 3 external LEDs using timer interrupt
+ The other LED is blinked in the main loop
+ */
 
 #include "pico/stdlib.h"
+#include <stdio.h>
 #define LEDRed 8
 #define LEDYellow 9
 #define LEDGreen 10
+#define LEDBlue 11
 
 static const uint32_t mask = 1U << LEDRed | 1U << LEDYellow | 1U << LEDGreen;
 
@@ -33,12 +37,22 @@ bool repeating_timer_callback(struct repeating_timer *t){
 
 int main() {
     const uint32_t delay = 500;
+    const uint32_t timeOn = 3000; // Time after the Blue LED is toggled in ms
+    absolute_time_t timeOff;
+
     gpio_init_mask(mask);
     gpio_set_dir_out_masked(mask);
     gpio_put_masked(mask, 0);
+    gpio_init(LEDBlue);
+    gpio_set_dir(LEDBlue, 1);
     struct repeating_timer timer;
     add_repeating_timer_ms(delay, repeating_timer_callback, NULL, &timer);
+    timeOff = make_timeout_time_ms(timeOn); // Absolute time after the Blue LED is toggled
+
     while (true) {
-        tight_loop_contents();
+        if(time_reached(timeOff)) {
+            gpio_xor_mask(1U<<LEDBlue);
+            timeOff = make_timeout_time_ms(timeOn); // Reset the time
+        }
     }
 }
