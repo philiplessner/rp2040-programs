fc2c3ce314d196760269fb7f1cd2c999391fbaa4
Philip Lessner <philiplessner@gmail.com>
2022-08-13 17:16:41 -0400

    Added Pico W micropython program to control leds from buttons in web browser and micropython program to send sensor reading to web browser

---
+++ Python/server_sensor/main.py
@@ -0,0 +1,108 @@
+import rp2
+import network
+import ubinascii
+import machine
+import urequests as requests
+import time
+import socket
+
+
+# Set up for internal temperature sensor
+sensor_temp = machine.ADC(4)
+conversion_factor = 3.3 / 65535
+
+# Set country to avoid possible errors
+rp2.country('US')
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+# If you need to disable powersaving mode
+# wlan.config(pm = 0xa11140)
+
+# See the MAC address in the wireless chip OTP
+mac = ubinascii.hexlify(network.WLAN().config('mac'),':').decode()
+print('mac = ' + mac)
+
+# Other things to query
+# print(wlan.config('channel'))
+# print(wlan.config('essid'))
+# print(wlan.config('txpower'))
+
+# Load login data from different file for safety reasons
+ssid = "ATT5Mwb2RC"
+pw = "8ieu2x4gd7ae"
+
+wlan.connect(ssid, pw)
+
+# Wait for connection with 10 second timeout
+timeout = 10
+while timeout > 0:
+    if wlan.status() < 0 or wlan.status() >= 3:
+        break
+    timeout -= 1
+    print('Waiting for connection...')
+    time.sleep(1)
+    
+# Handle connection error
+# Error meanings
+# 0  Link Down
+# 1  Link Join
+# 2  Link NoIp
+# 3  Link Up
+# -1 Link Fail
+# -2 Link NoNet
+# -3 Link BadAuth
+if wlan.status() != 3:
+    raise RuntimeError('Wi-Fi connection failed')
+else:
+    led = machine.Pin('LED', machine.Pin.OUT)
+    for i in range(wlan.status()):
+        led.on()
+        time.sleep(0.2)
+        led.off()
+        time.sleep(0.2)
+    print('Connected')
+    status = wlan.ifconfig()
+    print('ip = ' + status[0])
+    
+# Function to load in html page    
+def get_html(html_name):
+    with open(html_name, 'r') as file:
+        html = file.read()
+        
+    return html
+
+
+# HTTP server with socket
+addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+
+s = socket.socket()
+s.bind(addr)
+s.listen(1)
+
+print('Listening on', addr)
+
+# Listen for connections
+while True:
+    try:
+        cl, addr = s.accept()
+        print('Client connected from', addr)
+        cl_file = cl.makefile('rwb', 0)
+        while True:
+            line = cl_file.readline()
+            if not line or line == b'\r\n':
+                break
+            
+        response = get_html("./index.html")
+        reading = sensor_temp.read_u16() * conversion_factor
+        temperature = 27.0 - (reading - 0.706)/0.001721
+        Temperature = "{:.1f}\u00B0C".format(temperature)
+        print(Temperature)
+        response = response.replace('temperature', Temperature)
+        
+        cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
+        cl.send(response)
+        cl.close()
+    except OSError as e:
+        cl.close()
+        print('Connection closed')
---
+++ Python/server_sensor/secrets.py
@@ -0,0 +1,4 @@
+secrets = {
+    'ssid': 'ATT5Mwb2RC',
+    'pw': '8ieu2x4gd7ae'
+            }
