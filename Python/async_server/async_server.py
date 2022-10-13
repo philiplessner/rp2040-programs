0f873c548926848178f4b32a791fff9a33bea0ca
Philip Lessner <philiplessner@gmail.com>
2022-10-12 21:26:12 -0400

    Added logging. Added correct exception handling for asyncio.wait_for

--- Python/async_server/async_server.py
+++ Python/async_server/async_server.py
@@ -1,7 +1,9 @@
 import socket
 import time
 import uasyncio as asyncio
+import logging
 from heartbeat import heartbeat
+import time_convert as tc
 
 
 class AsyncHTTPServer():
@@ -11,25 +13,21 @@ class AsyncHTTPServer():
         self.port = port
         self.backlog = backlog
         self.timeout = timeout
-
-
+        self.logger = logging.getLogger("Server")
+        fmt = "%(asctime)s:%(levelname)s: %(message)s"
+        logging.basicConfig(level=logging.INFO,
+                            filename="server.log",
+                            format=fmt)        
+        
     async def get_header(self, reader):
-        data = ''
-        self.header = ''
-        while (data != '\r\n'):
-            try:
-                data = await asyncio.wait_for(reader.readline(), self.timeout)
-            except asyncio.TimeoutError:
-                print("Reader Timed Out!")
-                data = b''
-            if data == b'':
-                raise OSError
-            data = data.decode('utf-8')
-            self.header += data
-
+        request_line = await reader.readline()
+        while (await reader.readline() != b'\r\n'):
+           pass
+        self.header = request_line.decode('utf-8')
 
     def request_verb(self):
         lines = self.header.split('\n')
+        verb = lines[0].split()[0]
         return lines[0].split()[0]
 
 
@@ -54,45 +52,71 @@ class AsyncHTTPServer():
             with open(filename, 'r') as f:
                 content = f.read()
             response_header = 'HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n'
+            self.logger.info(f"Response Header: {' '.join(response_header.splitlines())}")
             custom = self.customize_html(content)
             response = response_header + custom
         except OSError:
             response = 'HTTP/1.0 404 NOT FOUND\r\n\r\n'
+            self.logger.info(f"Response Header: {' '.join(response.splitlines())}")
 
         writer.write(response.encode('utf-8'))
-        print("\nWrote:\n", response)
+        #print("\nWrote:\n", response)
         await writer.drain()
         writer.close()
         await writer.wait_closed()
-        print("***Client Disconnected***\n")
+        self.logger.info("Client Disconnected")
 
 
     async def serve_client(self, reader, writer):
         addr = writer.get_extra_info('peername')
-        print(f"***Client Connected*** from: {addr}")
-        await self.get_header(reader)
-        print(f"Received:\n {self.header}\nfrom {addr}")
-        if (self.request_verb() == 'GET'):
-            filename = self.get_filename()
-            print('Filename:', filename)
-            await self.get_request(filename, writer)
-
+        self.logger.info(f"Client Connected from: {addr}")
+        try:
+            request_line = await asyncio.wait_for(reader.readline(),
+                                                  self.timeout)
+            while (await reader.readline() != b'\r\n'):
+               pass
+            self.header = request_line.decode('utf-8')
+            self.logger.info(f"Received: {' '.join(self.header.splitlines())} from {addr}")
+            if (self.request_verb() == 'GET'):
+                filename = self.get_filename()
+                self.logger.info(f"Wrote: {filename}")
+                await self.get_request(filename, writer)
+        except asyncio.TimeoutError:
+            response = 'HTTP/1.0 500 Internal Server Error\r\n\r\n'
+            writer.write(response.encode('utf-8'))
+            self.logger.info(f"Wrote: {response}")
+            #print("\nWrote:\n", response)
+            await writer.drain()
+            writer.close()
+            await writer.wait_closed()
+            self.logger.info("Client Disconnected")
 
     async def main(self):
-        print("Setting Up Webserver")
+        self.logger.info("Setting Up Webserver")
         asyncio.create_task(heartbeat(500))
         self.server = await asyncio.start_server(self.serve_client, self.host, self.port, backlog=self.backlog)
         while True:
-            print("In Loop Listening...")
+            #print("In Loop Listening...")
             await asyncio.sleep(60)
+
+    def get_local_time(self):
+        tz = [ [         -5, 0, 0],  # Time offset  [      H,M,S] -5 US/Eastern 
+           [  3, 13, 1, 0, 0],  # Start of DST [ M,D, H,M,S] Mar 13
+           [ 11, 6, 2, 0, 0],  # End   of DST [ M,D, H,M,S] Nov 6
+           [         1, 0, 0]  # DST Adjust   [      H,M,S] +1 hour
+       ]
+        utc = tc.UtcTime()
+        loc = tc.LocalTime(tz)
+        current_time = f"{tc.Readable(loc)}"
+        self.logging.info(f"Current Date/Time: {current_time}")
+        return current_time
 
 
     async def close(self):
-        print('Closing server')
+        self.logger.info("Closing Server")
         self.server.close()
         await self.server.wait_closed()
-        print('Server closed')
-
+        self.logger.info("Server Closed")
 
     def run(self):
         try:
